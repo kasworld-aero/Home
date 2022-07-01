da4d2e1389753c820fdc572bbba98878ce2c0c05
Kasworld <32999892+kasworld-aero@users.noreply.github.com>
2022-07-01 20:58:54 +0530

    Create postck.js

---
+++ js/cdn/postck.js
@@ -0,0 +1,24 @@
+  checkAdBlocker();
+
+function checkAdBlocker() {
+    try {
+        fetch(
+            new Request("https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js", {
+                method: 'HEAD',
+                mode: 'no-cors'
+            })).catch(error => {
+            showNotification()
+        });
+    } catch (e) {
+        // Request failed, likely due to ad blocker
+        showNotification()
+    }
+}
+
+function showNotification() {
+ 
+  document.getElementById("postBody").innerHTML = '' + 
+'<blockquote class="blocked"><strong><svg width="24" height="24" viewBox="0 0 24 24" style="fill:currentColor;margin-right: 10px;"><path d="M11,15H13V17H11V15M11,7H13V13H11V7M12,2C6.47,2 2,6.5 2,12A10,10 0 0,0 12,22A10,10 0 0,0 22,12A10,10 0 0,0 12,2M12,20A8,8 0 0,1 4,12A8,8 0 0,1 12,4A8,8 0 0,1 20,12A8,8 0 0,1 12,20Z"></path></svg>Ad-Block Enabled</strong><p>Sorry, we detected that you have activated Ad-Blocker.For that, we hope that you disable it.</p><p>By disabling Ad-Blocker, you have helped us in developing this Website. Thank you</p></blockquote>' + 
+'';
+}
+ 
