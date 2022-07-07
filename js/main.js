eef3b2fbeb1f17f5fb7e9278f3a0693bb262dd8a
Kasworld <32999892+kasworld-aero@users.noreply.github.com>
2022-07-08 01:59:49 +0530

    Update main.js

--- js/main.js
+++ js/main.js
@@ -606,3 +606,78 @@ function showNotification() {
 })("undefined" != typeof window ? window : {});
 
 //end lazy
+
+
+// randome link add
+    
+let cnt= '450';
+var luck = Math.floor(Math.random()*cnt); script = document.createElement('script'); script.src = 'https://kasworld-aero.blogspot.com/feeds/posts/summary?start-index='+luck+'&max-results=1&alt=json-in-script&callback=showLucky'; script.type = 'text/javascript'; document.getElementsByTagName('head')[0].appendChild(script);function showLucky(root){ var feed = root.feed; var entries = feed.entry || []; var entry = feed.entry[0]; for (var j = 0; j < entry.link.length; ++j){if (entry.link[j].rel == 'alternate'){
+  document.getElementById('myLuckyPost').value = '<a href="'+entry.link[j].href+'">'+entry.link[j].title+'</a>'; 
+ setTimeout(() => {
+  document.getElementById('stform').click();
+}, 5000)
+
+   }}};
+      
+    
+  
+      
+  const taskAdder = document.querySelector('.taskAdder');
+const myTasks = document.querySelector('.myTasks');
+
+const tasks = JSON.parse(localStorage.getItem('taskList')) || [];
+
+taskAdder.addEventListener('submit', addTask);
+myTasks.addEventListener('click', toggleDone);
+outputTasks();
+
+
+function addTask(e){
+    e.preventDefault();
+    const textTask = this.querySelector('[name=task]').value;
+    const task = {
+        textTask,done:false
+    }
+
+    tasks.push(task);
+    saveStorage();
+    outputTasks();
+
+    this.reset();
+}
+
+function saveStorage(){
+  
+    localStorage.setItem('taskList',JSON.stringify(tasks));
+}
+
+function outputTasks() {
+    let html = tasks.map(function(data,i) {
+        let myClass = data.done ? 'done' : '';
+        return '<li data-index='+i+'><div class="'+myClass+'">'+
+            data.textTask+'<span class="remove">  X</span></div></li>';
+    })
+
+    myTasks.innerHTML = html.join('');
+}
+
+function toggleDone(e) {
+    
+    const myEl = e.target;
+    const mySel = myEl.parentElement;
+    if(myEl.className === 'remove') {
+        let index = mySel.parentElement.dataset.index;
+        let temp = tasks.splice(index,1);
+        console.log(temp);
+    } else {
+    
+   
+        myEl.classList.toggle('done');
+    
+        tasks[mySel.dataset.index].done = !tasks[mySel.dataset.index].done;
+    }
+
+    saveStorage();
+    outputTasks();
+}
+
