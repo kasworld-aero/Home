ed75fcd7725f775c8d8885d9af96b29ee3d9ee66
Kasworld <32999892+kasworld-aero@users.noreply.github.com>
2023-03-11 20:39:09 +0530

    Update 002.js

--- js/cdn/002.js
+++ js/cdn/002.js
@@ -46,7 +46,7 @@ function outputTasks() {
         let myClass = data.done ? 'done' : '';
         
         var elements = document.querySelectorAll(".dkal");
-     elements[i].innerHTML = '<div data-index='+i+'><div class="'+myClass+'">'+data.textTask+'<span class="remove">  X </span></div></div>';
+     elements[i].innerHTML = data.textTask;
     
         
         return '<li data-index='+i+'><div class="'+myClass+'">'+
