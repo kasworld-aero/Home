93f603c61f7327116675ce84e2ec897795acc65a
Kasworld <32999892+kasworld-aero@users.noreply.github.com>
2023-03-17 15:55:29 +0530

    Update aero.js

--- js/cdn/aero.js
+++ js/cdn/aero.js
@@ -1,1 +1,12 @@
 //This is only for Aero Sites
+function googleTranslateElementInit() {
+  new google.translate.TranslateElement({pageLanguage: 'en', layout: google.translate.TranslateElement.FloatPosition.TOP_LEFT, multilanguagePage: true, gaTrack: true, gaId: 'UA-104748378-1'}, 'google_translate_element');
+};
+setInterval(function() {
+if(document.body.style.top =="40px"){ document.body.style.top ="0px"};
+
+  // This will be executed every 5 seconds
+}, 5000);
+  
+  
+
